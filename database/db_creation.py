1796402b6ac6b1af9f90a11a251f69992a7a3083
Kapitan Avaria <67710267+Kapitan-Avaria@users.noreply.github.com>
2024-02-14 21:44:56 +0300

    Added db_creation.py

---
+++ database/db_creation.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine, Column, Integer, String, DECIMAL
+from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils import database_exists, create_database
+
+engine = create_engine("sqlite+pysqlite:///database.db")
+if not database_exists(engine.url):
+    create_database(engine.url)
+print(database_exists(engine.url))
+
+
+Base = declarative_base()
+
+
+class Address(Base):
+    __tablename__ = "addresses"
+
+    id = Column(Integer, primary_key=True)
+    latitude = Column(DECIMAL(9, 6))
+    longitude = Column(DECIMAL(9, 6))
+    string_address = Column(String)
+
+    def __repr__(self):
+        return f"<Address(lat={self.latitude}, lon={self.longitude}, string_address={self.string_address})>"
+
+
+Base.metadata.create_all(engine)
+
