f62f0e0064d9321f11852f835adda2dd83457e0d
Kapitan Avaria <67710267+Kapitan-Avaria@users.noreply.github.com>
2024-02-21 01:28:41 +0300

    Removed geocoding.py

--- geocoding/geocoding.py
+++
@@ -1,6 +0,0 @@
-from YandexClient import Client
-from constants import YANDEX_API_KEY
-
-client = Client(YANDEX_API_KEY)
-coords = client.coordinates("Кореновск г., Гагарина ул., 11")
-print(coords)
