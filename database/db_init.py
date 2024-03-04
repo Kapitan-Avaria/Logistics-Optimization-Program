12bb13b6883ab995951c79a99a70d025d9fd0cba
Kapitan Avaria <67710267+Kapitan-Avaria@users.noreply.github.com>
2024-03-04 17:21:21 +0300

    Added the rest of tables

--- database/db_init.py
+++ database/db_init.py
@@ -1,5 +1,4 @@
-import sqlalchemy as sa
-from sqlalchemy import create_engine, Column, Integer, Float, String, DECIMAL, Text, ForeignKey, UniqueConstraint
+from sqlalchemy import create_engine, Column, Integer, Float, String, DECIMAL, Text, JSON, Date, Time, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import declarative_base
 from sqlalchemy_utils import database_exists, create_database
 
@@ -22,7 +21,7 @@ class Address(Base):
     longitude = Column(DECIMAL(9, 6))
     string_address = Column(String, unique=True)
     machine_address = Column(String, unique=True)
-    delivery_zone = Column(Integer, ForeignKey("delivery_zones.id"))
+    delivery_zone_id = Column(Integer, ForeignKey("delivery_zones.id"))
     comment = Column(Text)
 
     __table_args__ = (UniqueConstraint('latitude', 'longitude', name='_address_coords_uc'),)
@@ -47,11 +46,75 @@ class SegmentStatistics(Base):
 
     segment_id = Column(Integer, ForeignKey("segments.id"), primary_key=True)
     record_id = Column(Integer, primary_key=True)
-
     route_duration = Column(Integer)
-    date = Column(sa.Date)
-    start_time = Column(sa.Time)
+    date = Column(Date)
+    start_time = Column(Time)
     week_day = Column(Integer)
+    json_response = Column(JSON)
+
+
+class DeliveryZone(Base):
+    __tablename__ = "delivery_zones"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True)
+    number = Column(String)
+    client_id = Column(Integer, ForeignKey("clients.id"))
+    address_id = Column(Integer, ForeignKey("addresses.id"))
+    date = Column(Date)
+    delivery_time_start = Column(Time)
+    delivery_time_end = Column(Time)
+
+
+class Client(Base):
+    __tablename__ = "clients"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+
+
+class Route(Base):
+    __tablename__ = "routes"
+
+    id = Column(Integer, primary_key=True)
+    vehicle_id = Column(Integer, ForeignKey("vehicles.id"))
+    route_data = Column(JSON)   # Упорядоченный список адресов доставки и время прохождения промежутков
+    date = Column(Date)
+    time_start = Column(Time)
+    time_end = Column(Time)
+
+
+class Vehicle(Base):
+    __tablename__ = "vehicles"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    category = Column(String)
+    dimensions = Column(JSON)   # payload: depth, width, height; outer_dimensions: ...
+    weight_capacity = Column(Integer)
+
+
+class Products(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    form_factor = Column(Integer, ForeignKey("form_factors.id"))
+    dimensions = Column(JSON)   # spatial dimensions in the format corresponding to the form_factor
+
+
+class FormFactor(Base):
+    __tablename__ = "form_factors"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    dimensions_template = Column(JSON)
 
 
 Base.metadata.create_all(engine)
