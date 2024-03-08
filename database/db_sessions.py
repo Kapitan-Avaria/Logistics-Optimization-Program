1f9e4b702b62ebcbe14f5e0c7297230470168420
Kapitan Avaria <67710267+Kapitan-Avaria@users.noreply.github.com>
2024-03-08 20:44:28 +0300

    Replaced 'get_existing_address()' function with general function 'select_existing_object()'

--- database/db_sessions.py
+++ database/db_sessions.py
@@ -1,13 +1,52 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.exc import NoResultFound
-from database.db_init import engine, Address
+from database.db_init import engine, Address, Order, DeliveryZone, Client, Product, OrderProduct
+from decimal import Decimal
+
+
+def insert_orders(orders: list[dict]):
+    with Session(bind=engine) as session:
+        for order in orders:
+            existing_order = select_existing_object(session, Order, number=order["number"])
+
+            existing_order.date = order["datetime"]
+
+            client = select_existing_object(session, Client, name=order["client"])
+            existing_order.client_id = client.id
+
+            address = select_existing_object(session, Address, string_address=order["address"])
+            if address.comment is None and order["address_comment"]:
+                address.comment = order["address_comment"]
+            if address.latitude is None and address.longitude is None and order["geo_location"]:
+                address.latitude = Decimal(order["geo_location"]["latitude"])
+                address.longitude = Decimal(order["geo_location"]["longitude"])
+            if address.delivery_zone_id is None and order["delivery-zone"]:
+                delivery_zone = select_existing_object(session, DeliveryZone, name=order["delivery-zone"])
+                address.delivery_zone_id = delivery_zone.id
+            existing_order.address_id = address.id
+
+            existing_order.delivery_time_start = order["delivery-time-start"]
+            existing_order.delivery_time_end = order["delivery-time-end"]
+
+            if "products" in order.keys():
+                for prd in order["products"]:
+                    product = select_existing_object(session, Product, name=prd["name"])
+                    product_in_order = select_existing_object(
+                        session,
+                        OrderProduct,
+                        order_id=existing_order.id,
+                        product_id=product.id
+                    )
+                    product_in_order.quantity = prd["quantity"]
+
+        session.commit()
 
 
 def insert_addresses(addresses):
     with Session(bind=engine) as session:
 
         for address_string in addresses:
-            get_existing_address(session, address_string)
+            select_existing_object(session, Address, string_address=address_string)
 
         session.commit()
 
@@ -15,7 +54,7 @@ def insert_addresses(addresses):
 def get_coords(address_string):
 
     with Session(bind=engine) as session:
-        existing_address = get_existing_address(session, address_string)
+        existing_address = select_existing_object(session, Address, string_address=address_string)
         
         coords = (existing_address.latitude, existing_address.longitude) \
             if (existing_address.latitude is not None and existing_address.latitude is not None) \
@@ -28,7 +67,7 @@ def get_coords(address_string):
 
 def insert_coords(address_string, coords):
     with Session(bind=engine) as session:
-        existing_address = get_existing_address(session, address_string)
+        existing_address = select_existing_object(session, Address, string_address=address_string)
 
         latitude, longitude = coords
 
@@ -38,19 +77,19 @@ def insert_coords(address_string, coords):
         session.commit()
 
 
-def get_existing_address(session, address_string):
-    address_string = regularize(address_string)
+def select_existing_object(session, class_name, **kwargs):
+    for key in kwargs.keys():
+        if isinstance(kwargs[key], str):
+            kwargs[key] = regularize(kwargs[key])
 
-    # Try to read given address from the table
-    # If not exists, add to the table
     try:
-        q = session.query(Address).filter_by(string_address=address_string)
-        existing_address = q.one()
+        q = session.query(class_name).filter_by(**kwargs)
+        existing_object = q.one()
     except NoResultFound:
-        new_address = Address(string_address=address_string)
-        session.add(new_address)
-        existing_address = new_address
-    return existing_address
+        new_object = class_name(**kwargs)
+        session.add(new_object)
+        existing_object = new_object
+    return existing_object
 
 
 def read_strings_input():
