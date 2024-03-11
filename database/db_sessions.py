8c7a8243934bea89781003ec73426efa6385343b
Kapitan Avaria <67710267+Kapitan-Avaria@users.noreply.github.com>
2024-03-11 23:43:00 +0300

    Added and completed functions: 'insert_products()', 'insert_vehicle_geodata()'. Fixed 'use_with_session()' decorator and signature of all affected functions

--- database/db_sessions.py
+++ database/db_sessions.py
@@ -1,15 +1,21 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.exc import NoResultFound
-from database.db_init import engine, Address, Order, DeliveryZone, Client, Product, OrderProduct, Vehicle
+from database.db_init import engine, Address, Order, DeliveryZone, Client, Product, OrderProduct, Vehicle, FormFactor, VehicleGeodata
 from decimal import Decimal
 
 
 def use_with_session(func):
-    """Decorator function that wraps the func with 'with Session() ... session.commit()'"""
+    """Decorator function that wraps the func with 'with Session() ... session.commit()',
+    if 'session' argument is not provided"""
 
     def wrapper(*args, **kwargs):
+        # If existing session provided, continues with default function behaviour
+        if "session" in kwargs.keys():
+            return func(*args, **kwargs)
+
+        # If the session is not provided, creates new one
         with Session(bind=engine) as session:
-            ret = func(func(session, *args, **kwargs))
+            ret = func(*args, **kwargs, session=session)
             session.commit()
         return ret
 
@@ -17,7 +23,7 @@ def use_with_session(func):
 
 
 @use_with_session
-def insert_orders(session, orders: list[dict]):
+def insert_orders(orders: list[dict], session: Session = None):
     for order in orders:
         existing_order: Order = select_existing_object(session, Order, number=order["number"])
 
@@ -53,13 +59,13 @@ def insert_orders(session, orders: list[dict]):
 
 
 @use_with_session
-def insert_addresses(session, addresses: list):
+def insert_addresses(addresses: list, session: Session = None):
     for address_string in addresses:
         select_existing_object(session, Address, string_address=address_string)
 
 
 @use_with_session
-def insert_vehicles(session, vehicles: list[dict]):
+def insert_vehicles(vehicles: list[dict], session: Session = None):
     for vehicle in vehicles:
         existing_vehicle: Vehicle = select_existing_object(session, Vehicle, name=vehicle["name"])
         existing_vehicle.category = vehicle["category"]
@@ -68,12 +74,31 @@ def insert_vehicles(session, vehicles: list[dict]):
 
 
 @use_with_session
-def insert_products(session, products: list[dict]):
-    pass
+def insert_products(products: list[dict], session: Session = None):
+    for product in products:
+        existing_product: Product = select_existing_object(session, Product, name=product["name"])
+        form_factor: FormFactor = select_existing_object(session, FormFactor, name=product["form-factor"])
+        existing_product.form_factor = form_factor.id
+        existing_product.dimensions = product["dimensions"]
 
 
 @use_with_session
-def get_coords(session, address_string):
+def insert_vehicle_geodata(data, session: Session = None):
+    for vehicle_data in data:
+        existing_vehicle: Vehicle = select_existing_object(session, Vehicle, name=vehicle_data["vehicle"])
+        for record in vehicle_data["geodata"]:
+            new_data = VehicleGeodata(
+                vehicle_id=existing_vehicle.id,
+                datetime=record["datetime"],
+                latitude=Decimal(record["latitude"]),
+                longitude=Decimal(record["longitude"])
+            )
+            session.add(new_data)
+
+
+
+@use_with_session
+def get_coords(address_string, session: Session = None):
     existing_address = select_existing_object(session, Address, string_address=address_string)
 
     coords = (existing_address.latitude, existing_address.longitude) \
@@ -84,7 +109,7 @@ def get_coords(session, address_string):
 
 
 @use_with_session
-def insert_coords(session, address_string, coords):
+def insert_coords(address_string, coords, session: Session = None):
     existing_address = select_existing_object(session, Address, string_address=address_string)
 
     latitude, longitude = coords
@@ -105,19 +130,20 @@ def select_existing_object(session, class_name, **kwargs):
         new_object = class_name(**kwargs)
         session.add(new_object)
         existing_object = new_object
+    session.commit()
     return existing_object
 
 
 def read_strings_input():
     arr = []
-    
+
     s = input()
-    
+
     while s != '':
         arr.append(s)
         s = input()
     return arr
-    
+
 
 def regularize(s: str):
     # Заменяем нестандартные пробельные символы обычным пробелом
