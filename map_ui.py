009e2aff758a35557ec02997a0b0f3afe55bdfcf
Kapitan Avaria <67710267+Kapitan-Avaria@users.noreply.github.com>
2024-04-04 00:52:36 +0300

    Added test ui for drawing segments

--- map_ui.py
+++ map_ui.py
@@ -4,7 +4,18 @@ import sys
 import os
 import pygame
 
-from database.db_sessions import calc_direct_distances
+from database.db_sessions import calc_direct_distances, get_all_segments, get_many_coords_from_db_addresses
+
+
+map_w = 600
+map_h = 450
+
+center_lat = 44.987749
+center_lon = 38.957643
+spn_lon = 1
+spn_lat = 1
+
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={center_lon},{center_lat}&spn={spn_lon},{spn_lat}&size=450,450&l=map"
 
 
 def get_map_photo(map_request):
@@ -14,19 +25,26 @@ def get_map_photo(map_request):
         print(map_request)
         print("Http статус:", response.status_code, "(", response.reason, ")")
         sys.exit(1)
-    map_file = "map.png"
+    content = response.content
+    return content
+
+
+def draw_map(content, map_file="map.png"):
     with open(map_file, "wb") as file:
-        file.write(response.content)
+        file.write(content)
+
     pygame.init()
     screen = pygame.display.set_mode((map_w, map_h))
     screen.blit(pygame.image.load(map_file), (0, 0))
 
     pygame.draw.circle(screen, 'black', geocoord_to_map_pixels(np.array([center_lon, center_lat])), 5)
-    pygame.draw.circle(screen, 'red', geocoord_to_map_pixels(np.array([center_lon-0.5, center_lat+0.5])), 5)
-    pygame.draw.circle(screen, 'green', geocoord_to_map_pixels(np.array([center_lon+0.5, center_lat-0.5])), 5)
-    pygame.draw.circle(screen, 'blue', geocoord_to_map_pixels(np.array([center_lon-0.5, center_lat-0.5])), 5)
-    pygame.draw.circle(screen, 'magenta', geocoord_to_map_pixels(np.array([center_lon+0.5, center_lat+0.5])), 5)
+    pygame.draw.circle(screen, 'red', geocoord_to_map_pixels(np.array([center_lon - 0.5, center_lat + 0.5])), 5)
+    pygame.draw.circle(screen, 'green', geocoord_to_map_pixels(np.array([center_lon + 0.5, center_lat - 0.5])), 5)
+    pygame.draw.circle(screen, 'blue', geocoord_to_map_pixels(np.array([center_lon - 0.5, center_lat - 0.5])), 5)
+    pygame.draw.circle(screen, 'magenta', geocoord_to_map_pixels(np.array([center_lon + 0.5, center_lat + 0.5])), 5)
 
+    pygame.draw.circle(screen, 'red', geocoord_to_map_pixels(np.array([38.997269, 45.802001])), 5)
+    pygame.draw.circle(screen, 'red', geocoord_to_map_pixels(np.array([39.727067, 44.465523])), 5)
 
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
@@ -51,29 +69,67 @@ def geocoord_to_map_pixels(coords):
         np.array([center_lon, coords[1],  center_lon, center_lon]),
         np.array([coords[0],  coords[0],  center_lat, coords[0]]),
         np.array([center_lon, coords[1],  coords[1],  coords[1]])
-    ) / np.array([center_lat - coords[0], center_lat - coords[0], center_lon - coords[1], center_lon - coords[1]])
+    ) / np.abs(np.array([center_lat - coords[0], center_lat - coords[0], center_lon - coords[1], center_lon - coords[1]]))
 
     # [vert, hor]
     m_per_deg = np.array([m_per_deg[:2].mean(), m_per_deg[2:].mean()])
 
-
+    # coords_from_center = coords - np.array([center_lon, center_lat])
+    # pixel_coords = np.array([map_w, map_h]) / 2 + coords_from_center * np.array([map_h, map_h]) * m_per_deg / m_per_deg_center
 
     # top_left = np.array([center_lon, center_lat]) + np.array([-spn_lon, spn_lat])
-    # pixel_coords = (coords - top_left) / np.array([2 * spn_lon, -2 * spn_lat]) * np.array([map_h, map_h])
+    # coords_from_tl = (coords - top_left)
+    # pixel_coords = coords_from_tl / np.array([2 * spn_lon, -2 * spn_lat]) * np.array([map_h, map_h]) #* m_per_deg / m_per_deg_center
 
-    # return pixel_coords
+    coords_from_center = (coords - np.array([center_lon, center_lat]))
+    pixel_coords = (np.array([spn_lon, -spn_lat]) + coords_from_center * m_per_deg / m_per_deg_center) / np.array([2 * spn_lon, -2 * spn_lat]) * np.array([map_h, map_h])  # * m_per_deg / m_per_deg_center
+
+    return pixel_coords
 
 
-map_w = 600
-map_h = 450
+def geocoord_to_map_pixels_rough(coords: np.ndarray):
+    coords_from_center = (coords - np.array([center_lon, center_lat]))
+    # pixel_coords = np.array([map_w, map_h]) / 2 + coords_from_center * map_h
+    pixel_coords = (np.array([spn_lon, -spn_lat]) + coords_from_center) / np.array([2 * spn_lon, -2 * spn_lat]) * np.array([map_h, map_h]) / 4 + 200
+    return pixel_coords
 
-center_lat = 44.987749
-center_lon = 38.957643
-spn_lon = 1
-spn_lat = 1
 
-map_request = f"http://static-maps.yandex.ru/1.x/?ll={center_lon},{center_lat}&spn={spn_lon},{spn_lat}&l=map"
-get_map_photo(map_request)
+def draw_map_rough():
+    pygame.init()
+    screen = pygame.display.set_mode((map_w, map_h))
 
-# print(geocoord_to_map_pixels(np.array([center_lat, center_lon])))
-# print(geocoord_to_map_pixels(np.array([center_lat+0.5, center_lon+0.5])))
+    distances = dict()
+    coords_dict = dict()
+
+    segments = get_all_segments()
+    for segment in segments:
+        id_1 = segment["address_1_id"]
+        id_2 = segment["address_2_id"]
+        coords_dict[id_1] = None
+        coords_dict[id_2] = None
+        distances[(id_1, id_2)] = segment["direct_distance"]
+
+    coords_dict = get_many_coords_from_db_addresses(coords_dict.keys())
+    pixel_coords = geocoord_to_map_pixels_rough(np.array(list(coords_dict.values()))).tolist()
+    pixel_coords_dict = dict(zip(coords_dict.keys(), pixel_coords))
+
+    max_dist = max(distances.values())
+    min_dist = min(distances.values())
+
+    for (id_1, id_2) in distances.keys():
+        shade = max(8, int((1 - (distances[(id_1, id_2)] - min_dist) / (max_dist - min_dist)) * 255))
+        pygame.draw.line(screen, (shade, 0, shade), pixel_coords_dict[id_1], pixel_coords_dict[id_2])
+    pygame.draw.circle(screen, 'yellow', geocoord_to_map_pixels_rough(np.array([center_lon, center_lat])), 5)
+    pygame.draw.circle(screen, 'yellow', geocoord_to_map_pixels(np.array([38.997269, 45.802001])), 5)
+    pygame.draw.circle(screen, 'yellow', geocoord_to_map_pixels(np.array([39.727067, 44.465523])), 5)
+
+
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
+    pass
+
+
+draw_map_rough()
+
