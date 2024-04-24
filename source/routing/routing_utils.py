12e19bbdf1e85c82299a0653a9259b041d13fb2a
Kapitan Avaria <67710267+Kapitan-Avaria@users.noreply.github.com>
2024-04-24 16:49:51 +0300

    Refactored

--- source/routing/routing_utils.py
+++ source/routing/routing_utils.py
@@ -2,11 +2,12 @@ from datetime import datetime
 
 
 def unix_time_from_hour(hour):
-        dt = datetime.strptime(f"{hour}:00", "%H:%M")
-        dt_now = datetime.now()
-        dt = dt.replace(year=dt_now.year, month=dt_now.month, day=dt_now.day)
-        unix_time = int(dt.timestamp())
-        return unix_time
+    dt = datetime.strptime(f"{hour}:00", "%H:%M")
+    dt_now = datetime.now()
+    dt = dt.replace(year=dt_now.year, month=dt_now.month, day=dt_now.day)
+    unix_time = int(dt.timestamp())
+    return unix_time
+
 
 def get_unix_time_now(shift=30):
-        return datetime.now().timestamp() + shift
+    return datetime.now().timestamp() + shift
