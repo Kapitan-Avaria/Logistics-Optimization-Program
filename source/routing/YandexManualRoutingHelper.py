56f3422153c4f4bdc5281099cdb775e8a370fc37
nikola-laptop <captaincrash.artist@gmail.com>
2024-04-25 16:20:47 +0300

    Fixed

--- source/routing/YandexManualRoutingHelper.py
+++ source/routing/YandexManualRoutingHelper.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from time import sleep
-from yandex_in_browser_routing_tools import extract_route_distance, extract_route_duration
+from yandex_in_browser_routing_tools import insert_data
 
 
 
@@ -11,20 +11,9 @@ class YandexManualRoutingHelper:
             sleep(0.2)
             time_row = input('>>> ')
             input()
-            distance_row = input()
+            dist_row = input()
             print()
-            sleep(0.2)
-            
-            route_distance = extract_route_distance(distance_row)
-            route_duration = extract_route_duration(time_row)
 
-            insert_segment_statistics(
-                segment_id=segment_id,
-                route_distance=route_distance,
-                route_duration=route_duration,
-                date=date,
-                start_time=datetime.strptime(f'{t}:00', '%H:%M').time(),
-                week_day=date.weekday(),
-                json_response=None,
-            )
+            insert_data(segment_id, dist_row, time_row, date, t)
+            sleep(0.2)
 
