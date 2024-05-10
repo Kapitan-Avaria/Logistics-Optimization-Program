ff957e54c0e65b95b577ae5822427a18b8694bad
Kapitan Avaria <67710267+Kapitan-Avaria@users.noreply.github.com>
2024-05-10 15:15:08 +0300

    Added type hints

--- source/load_config.py
+++ source/load_config.py
@@ -4,6 +4,6 @@ from json import load
 with open("config.cfg", 'r') as f:
     config = load(f)
 
-YANDEX_GEO_API_KEY = config["YANDEX_GEO_API_KEY"]
-YANDEX_ROUTING_API_KEY = config["YANDEX_ROUTING_API_KEY"]
-ORS_ROUTING_API_KEY = config["ORS_ROUTING_API_KEY"]
+YANDEX_GEO_API_KEY: str = config["YANDEX_GEO_API_KEY"]
+YANDEX_ROUTING_API_KEY: str = config["YANDEX_ROUTING_API_KEY"]
+ORS_ROUTING_API_KEY: str = config["ORS_ROUTING_API_KEY"]
