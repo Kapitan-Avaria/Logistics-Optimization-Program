1327650c743137c9a98e1141d55d7708a1c428c0
nikola-laptop <captaincrash.artist@gmail.com>
2024-07-11 15:38:26 +0300

    Removed junk

--- source/database/data_test.py
+++ source/database/data_test.py
@@ -2,12 +2,10 @@ import pandas as pd
 from db_queries import get_objects
 from db_init import SegmentStatistics
 
-
-if __name__ == '__main__':
-    seg_stats = get_objects(class_name=SegmentStatistics)
-    df = pd.json_normalize(seg_stats)
-    df["avg_velocity"] = df["distance"] / (df["duration"] * 60)
-    seg_id = 11
-    df_seg = df[df["segment_id"] == seg_id]
-    df_seg.loc[:, ["avg_velocity"]] = (df_seg["avg_velocity"]-df_seg["avg_velocity"].min())/(df_seg["avg_velocity"].max()-df_seg["avg_velocity"].min())
-    df_seg.loc[df_seg["segment_id"] == seg_id][["start_time", "avg_velocity"]].groupby("start_time").mean().plot(kind='bar')
+seg_stats = get_objects(class_name=SegmentStatistics)
+df = pd.json_normalize(seg_stats)
+df["avg_velocity"] = df["distance"] / (df["duration"] * 60)
+seg_id = 11
+df_seg = df[df["segment_id"] == seg_id]
+df_seg.loc[:, ["avg_velocity"]] = (df_seg["avg_velocity"]-df_seg["avg_velocity"].min())/(df_seg["avg_velocity"].max()-df_seg["avg_velocity"].min())
+df_seg.loc[df_seg["segment_id"] == seg_id][["start_time", "avg_velocity"]].groupby("start_time").mean().plot(kind='bar')
