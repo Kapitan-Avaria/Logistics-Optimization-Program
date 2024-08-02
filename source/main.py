fcdd14dc3fd163cbed64276de45fd47050ac49bf
nikola-laptop <captaincrash.artist@gmail.com>
2024-08-02 17:28:05 +0300

    Started to implement main Flask application

--- source/main.py
+++ source/main.py
@@ -6,17 +6,30 @@ from routing.routing_wrapper import RoutingWrapper
 from vrp.vrp_wrapper import VRPWrapper
 from pathlib import Path
 
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
+    app.run(debug=True)
+
     db_path = Path('..').resolve() / 'database.db'
     url_1c = ''
 
     # Initialize main clients and wrappers
-    client1c = HTTPClient1C(url_1c)
-    geocoder = GeocodingWrapper()
-    router = RoutingWrapper()
-    vrp = VRPWrapper()
+    # client1c = HTTPClient1C(url_1c)
+    # geocoder = GeocodingWrapper()
+    # router = RoutingWrapper()
     db_is_empty = db_init(db_path)
+
+    vrp_wrapper = VRPWrapper()
 
 
 
