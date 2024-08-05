716ec94619d2e8faa74d52b1e1e8f0e3b2b9e32f
nikola-laptop <captaincrash.artist@gmail.com>
2024-08-05 12:41:58 +0300

    Added comments and moved time sleep inside the 'get_coordinates' method

--- source/geocoding/geocoding_wrapper.py
+++ source/geocoding/geocoding_wrapper.py
@@ -12,20 +12,23 @@ class GeocodingWrapper:
             "yandex": YandexGeoClient(YANDEX_GEO_API_KEY)
         }
     
-    def get_coordinates(self, address):
-        address_object = get_objects(class_name=Address, address_string=address)[0]
+    def get_coordinates(self, address_string, time_sleep_seconds=1):
+        # Get the address object from the db
+        address_object = get_objects(class_name=Address, address_string=address_string)[0]
+        # Get coords from address if exist
         coords = (float(address_object['longitude']), float(address_object['latitude'])) \
             if (address_object['longitude'] is not None and address_object['latitude'] is not None) \
             else None
 
+        # Get coords from geocoder only if there are no coords in the address
         if coords is None:
-            coords = self.geo_clients["yandex"].get_coordinates(address)
-            insert_coords(address, coords)
+            coords = self.geo_clients["yandex"].get_coordinates(address_string)
+            sleep(time_sleep_seconds)
 
         return coords
 
-    def geocode_bulk(self, addresses, time_interval_seconds=1):
-        for address in addresses:
-            coords = self.get_coordinates(address)
-            sleep(time_interval_seconds)
+    def geocode_bulk(self, addresses, time_sleep_seconds=1):
+        for address_string in addresses:
+            coords = self.get_coordinates(address_string, time_sleep_seconds)
+            insert_coords(address_string, coords)
 
