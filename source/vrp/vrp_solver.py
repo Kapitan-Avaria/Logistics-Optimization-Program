ab7d07b244ee4f6255588e7abfabbf366d70e5b5
nikola-laptop <captaincrash.artist@gmail.com>
2024-08-06 18:10:08 +0300

    Added penalty function for big angular changes in course

--- source/vrp/vrp_solver.py
+++ source/vrp/vrp_solver.py
@@ -13,7 +13,7 @@ class CVRPTW:
         self.calc_base_distance = self.default_distance_evaluator if distance_evaluator is None else distance_evaluator
 
     def default_distance_evaluator(self, from_node, to_node):
-        return np.linalg.norm(np.array(self.locations[from_node]) - np.array(self.locations[to_node]))
+        return np.linalg.norm(np.array(self.locations[from_node]) - np.array(self.locations[to_node])) * 1000
 
     def time_dependent_travel_time(self, from_node, to_node, current_time):
         base_distance = self.calc_base_distance(from_node, to_node) / 1000  # Converting to kilometers
@@ -34,8 +34,24 @@ class CVRPTW:
 
         return base_distance / velocity
 
+    def deviation_penalty(self, from_node, to_node):
+        # Penal the vehicle if it's angular jumping to far away from the current location
+        loc0 = self.locations[0]
+        loc1 = self.locations[from_node]
+        loc2 = self.locations[to_node]
+        angle1 = np.arctan2(loc2[1] - loc0[1], loc2[0] - loc0[0])
+        angle2 = np.arctan2(loc1[1] - loc0[1], loc1[0] - loc0[0])
+        angle = abs(angle2 - angle1) % np.pi
+        if angle > np.pi / 8:
+            print(angle)
+            return (angle - np.pi / 8) / (np.pi - np.pi / 8) * 5
+        else:
+            return 0
+
     def travel_cost(self, from_node, to_node, current_time):
-        cost = self.time_dependent_travel_time(from_node, to_node, current_time)
+        travel_time = self.time_dependent_travel_time(from_node, to_node, current_time)
+        deviation_penalty = self.deviation_penalty(from_node, to_node)
+        cost = travel_time + deviation_penalty
         return cost
 
     def initial_solution(self):
@@ -44,7 +60,7 @@ class CVRPTW:
             routes.append([])
         return routes
 
-    def add_to_route(self, route, location, current_time, vehicle_load, vehicle_capacity):
+    def try_add_to_route(self, route, location, current_time, vehicle_load, vehicle_capacity):
         # Calc arrival time
         if not route:
             arrival_time = self.time_dependent_travel_time(0, location, current_time)
@@ -74,10 +90,19 @@ class CVRPTW:
 
         # If all conditions are satisfied, add load to the vehicle and append route with the location
         vehicle_load += total_volume
-        route.append((location, current_time, vehicle_load, wait_time))
+        # route.append((location, current_time, vehicle_load, wait_time))
         return current_time, vehicle_load, wait_time, True
 
-    def construct_routes(self):
+    def construct_routes(self, solver="greedy"):
+        if solver == "greedy":
+            routes = self.construct_routes_greedy()
+        elif solver == "greedy2":
+            routes = self.construct_routes_greedy2()
+        else:
+            raise ValueError("Invalid solver specified.")
+        return routes
+
+    def construct_routes_greedy(self):
         routes = self.initial_solution()
         unvisited = set(range(1, len(self.locations)))
         vehicle_loads = [0] * self.vehicle_count
@@ -107,9 +132,61 @@ class CVRPTW:
                 for loc in feasible_locations:
                     current_time = vehicle_times[v]
                     vehicle_load = vehicle_loads[v]
-                    new_time, new_load, wait_time, success = self.add_to_route(routes[v], loc, current_time,
-                                                                               vehicle_load, self.vehicle_capacities[v])
+                    new_time, new_load, wait_time, success = self.try_add_to_route(routes[v], loc, current_time,
+                                                                                   vehicle_load, self.vehicle_capacities[v])
+                    routes[v].append((loc, new_time, new_load, wait_time))
                     if success:
+                        vehicle_times[v] = new_time
+                        vehicle_loads[v] = new_load
+                        unvisited.remove(loc)
+                        progress = True
+                        print(
+                            f"Vehicle {v + 1} added location {loc} with arrival time {new_time:.2f}, "
+                            f"wait time {wait_time:.2f}, and load {new_load:.2f}")
+                        break
+
+            if not progress:
+                print("No progress made, breaking out of loop.")
+                break  # Exit if no progress is made
+
+        return routes
+
+    def construct_routes_greedy2(self):
+        routes = self.initial_solution()
+        unvisited = set(range(1, len(self.locations)))
+        vehicle_loads = [0] * self.vehicle_count
+        vehicle_times = [0] * self.vehicle_count
+
+        while unvisited:
+            progress = False
+            for v in range(self.vehicle_count):
+                # If all locations are visited, break the loop
+                if not unvisited:
+                    break
+
+                # Select the locations to which the vehicle can deliver all demanded products
+                feasible_locations = [
+                    loc for loc in unvisited
+                    if vehicle_loads[v] + sum(
+                        self.demands[loc][product] * self.product_volumes[product] for product in self.demands[loc]) <=
+                       self.vehicle_capacities[v]
+                ]
+                # Sort feasible locations by travel cost from current point
+                feasible_locations.sort(
+                    key=lambda loc: self.travel_cost(routes[v][-1][0], loc, vehicle_times[v]) if routes[v]
+                    else self.travel_cost(0, loc, vehicle_times[v])
+                )
+
+                # Try to add the closest feasible location to the vehicle route
+                for loc in feasible_locations:
+                    current_time = vehicle_times[v]
+                    vehicle_load = vehicle_loads[v]
+                    new_time, new_load, wait_time, success = self.try_add_to_route(routes[v], loc, current_time,
+                                                                                   vehicle_load,
+                                                                                   self.vehicle_capacities[v])
+                    routes[v].append((loc, new_time, new_load, wait_time))
+                    if success:
+
                         vehicle_times[v] = new_time
                         vehicle_loads[v] = new_load
                         unvisited.remove(loc)
@@ -138,7 +215,7 @@ if __name__ == "__main__":
     # Generate Example Data
     random.seed(42)
     num_locations = 50
-    num_vehicles = 10
+    num_vehicles = 5
     locations = [(25, 25)] + [(random.randint(0, 50), random.randint(0, 50)) for _ in range(1, num_locations)]
     demands = [{'A': random.randint(1, 5), 'B': random.randint(1, 5)} for _ in range(num_locations)]
     demands[0] = {}  # Depot has no demand
@@ -160,18 +237,18 @@ if __name__ == "__main__":
         'red',
         'blue',
         'gray',
+        'green',
+        'pink',
+        'darkgreen',
         'darkred',
         'lightred',
         'orange',
         'beige',
-        'green',
-        'darkgreen',
         'lightgreen',
         'darkblue',
         'lightblue',
         'purple',
         'darkpurple',
-        'pink',
         'cadetblue',
         'lightgray',
         'black'
