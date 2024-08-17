f9912aefeefc47e73ae1d43b83fb3caa14c50740
nikola-laptop <captaincrash.artist@gmail.com>
2024-08-17 12:18:16 +0300

    Added handling of ConnectionError

--- source/client_1c/http_client_1c.py
+++ source/client_1c/http_client_1c.py
@@ -6,27 +6,45 @@ class HTTPClient1C:
         """url: [ip-address]/[name-of-publication]"""
         self.url = url + '/hs/'
 
+    @staticmethod
+    def safe_request(request_func):
+        def wrapper(*args, **kwargs):
+            try:
+                return request_func(*args, **kwargs)
+            except requests.exceptions.ConnectionError:
+                print(f"Connection error: Can't connect to 1C HTTP-service via {request_func.__name__}")
+                return {}
+        return wrapper
+
+    @safe_request
     def get_archived_orders(self):
         return requests.get(self.url + 'orders/get_archived_orders').json()["orders"]
 
+    @safe_request
     def get_available_orders(self):
         return requests.get(self.url + 'orders/get_available_orders').json()["orders"]
 
+    @safe_request
     def get_all_products(self):
         return requests.get(self.url + 'products/get_all_products').json()["products"]
 
+    @safe_request
     def get_product(self, name):
         return requests.get(self.url + f'products/get_product/{name}').json()["products"]
 
+    @safe_request
     def get_all_vehicles(self):
         return requests.get(self.url + 'vehicles/get_all_vehicles').json()["vehicles"]
 
+    @safe_request
     def get_vehicle(self, name):
         return requests.get(self.url + f'vehicles/get_vehicle/{name}').json()["vehicles"]
 
+    @safe_request
     def get_available_vehicles(self):
         return requests.get(self.url + 'vehicles/get_available_vehicles').json()["vehicles"]
 
+    @safe_request
     def get_vehicles_geodata(self, date):
         return requests.get(self.url + f'vehicles/get_geodata/{date}').json()["data"]
 
