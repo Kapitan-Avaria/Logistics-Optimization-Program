62bc37b3be9dea0365fac603927beb69943e5b17
nikola-laptop <captaincrash.artist@gmail.com>
2024-08-22 16:03:13 +0300

    Made config.py to be more like interface to config.cfg

--- source/config.py
+++ source/config.py
@@ -1,4 +1,4 @@
-from json import load
+import json
 from pathlib import Path
 
 
@@ -7,6 +7,7 @@ class Config:
     YANDEX_ROUTING_API_KEY: str
     ORS_ROUTING_API_KEY: str
     DB_PATH: Path
+    URL_1C: str
     DEPOT_ADDRESS: str
 
     DEFAULT_SHIFT_START_B: str
@@ -15,6 +16,7 @@ class Config:
     DEFAULT_SHIFT_DURATION_C: str
     ALLOW_OVERNIGHT_ROUTES_B: bool
     ALLOW_OVERNIGHT_ROUTES_C: bool
+    ACTUAL_VOLUME_RATIO: float
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, 'instance'):
@@ -22,23 +24,38 @@ class Config:
         return cls.instance
 
     def __init__(self):
-        self.load()
-
-    def load(self):
         path = Path('').resolve()
         if (path / 'source').exists():
             path = path / 'source'
         else:
             while path.name != 'source':
                 path = path.parent
+        self.path = path
+        self.load()
 
-        with open(path / "config.cfg", 'r', encoding='utf8') as f:
-            config = load(f)
+    def __setattr__(self, key, value):
+        super().__setattr__(key, value)
+        with open(self.path / "config.cfg", 'r+', encoding='utf8') as f:
+            data = json.load(f)
+            if key not in data.keys():
+                return
+            if data[key] == value:
+                return
+            data[key] = value
+            f.seek(0)
+            new_data = json.dumps(data, indent=2)
+            f.write(new_data)
+            f.truncate()
+
+    def load(self):
+        with open(self.path / "config.cfg", 'r', encoding='utf8') as f:
+            config = json.load(f)
 
         self.YANDEX_GEO_API_KEY: str = config["YANDEX_GEO_API_KEY"]
         self.YANDEX_ROUTING_API_KEY: str = config["YANDEX_ROUTING_API_KEY"]
         self.ORS_ROUTING_API_KEY: str = config["ORS_ROUTING_API_KEY"]
         self.DB_PATH = Path(config["DB_PATH"])
+        self.URL_1C: str = config["URL_1C"]
         self.DEPOT_ADDRESS: str = config["DEPOT_ADDRESS"]
 
         self.DEFAULT_SHIFT_START_B: str = config["DEFAULT_SHIFT_START_B"]
@@ -47,4 +64,5 @@ class Config:
         self.DEFAULT_SHIFT_DURATION_C: str = config["DEFAULT_SHIFT_DURATION_C"]
         self.ALLOW_OVERNIGHT_ROUTES_B: bool = config["ALLOW_OVERNIGHT_ROUTES_B"]
         self.ALLOW_OVERNIGHT_ROUTES_C: bool = config["ALLOW_OVERNIGHT_ROUTES_C"]
+        self.ACTUAL_VOLUME_RATIO: float = config["ACTUAL_VOLUME_RATIO"]
 
